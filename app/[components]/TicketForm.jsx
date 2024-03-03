7f6788651436063270ada776e1b4cd32ac746df7
Pradikshan <pradikshan234@gmail.com>
2024-03-03 14:14:46 +0530

    Set up state

--- app/[components]/TicketForm.jsx
+++ app/[components]/TicketForm.jsx
@@ -4,6 +4,17 @@ import { useRouter } from "next/navigation";
 import React, { useState } from "react";
 
 const TicketForm = () => {
+  const startingTicketData = {
+    title: "",
+    description: "",
+    priority: 1,
+    progress: 0,
+    status: "Not started",
+    category: "hardware problem",
+  };
+
+  const [formData, setFormData] = useState(startingTicketData);
+
   return <div>TicketForm</div>;
 };
 
