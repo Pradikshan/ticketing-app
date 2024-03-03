98832801d413aa133b6347e821bcd8583ad82437
Pradikshan <pradikshan234@gmail.com>
2024-03-03 14:05:31 +0530

    Created ticket schema

--- app/[models]/Ticket.js
+++ app/[models]/Ticket.js
@@ -2,3 +2,18 @@ import mongoose, { Schema } from "mongoose";
 
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise;
+
+const ticketSchema = new Schema(
+  {
+    title: String,
+    description: String,
+    category: String,
+    priority: Number,
+    progress: Number,
+    status: String,
+    active: Boolean,
+  },
+  {
+    timestamps: true,
+  }
+);
