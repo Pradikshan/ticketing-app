819f3ce4ba42e25db0b7fda0fefb5da263c05f90
Pradikshan <pradikshan234@gmail.com>
2024-03-03 16:06:53 +0530

    Added error statements for debugging

--- app/api/Tickets/route.js
+++ app/api/Tickets/route.js
@@ -2,13 +2,22 @@ import Ticket from "../../[models]/Ticket";
 import { NextResponse } from "next/server";
 
 export async function POST(req) {
+  console.log("POST Request Received");
+
   try {
     const body = await req.json();
+    // console.log("Request Body:", body);
+
     const ticketData = body.formData;
+    // console.log("Ticket Data:", ticketData);
+
     await Ticket.create(ticketData);
+
+    console.log("Ticket created successfully");
 
     return NextResponse.json({ message: "Ticket created" }, { status: 201 });
   } catch (error) {
+    console.error("Error:", error);
     return NextResponse.json({ message: "Error: ", error }, { status: 500 });
   }
 }
