4c3d323bbc83997a0b882724bdb3109fcc93f33c
Pradikshan <pradikshan234@gmail.com>
2024-03-03 16:59:42 +0530

    Made minor change

--- app/[components]/StatusDisplay.jsx
+++ app/[components]/StatusDisplay.jsx
@@ -18,7 +18,11 @@ const StatusDisplay = ({ status }) => {
   };
 
   return (
-    <span className="inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 bg-green-200">
+    <span
+      className={`inline-block rounded-full px-2 py-1 text-xs font-semibold text-gray-700 ${getColor(
+        status
+      )}`}
+    >
       {status}
     </span>
   );
