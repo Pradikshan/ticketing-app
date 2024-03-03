38f99c9124e632a9384fa6696c96ba5ca233a73d
Pradikshan <pradikshan234@gmail.com>
2024-03-03 12:47:13 +0530

    imported priority display component

--- app/[components]/TicketCard.jsx
+++ app/[components]/TicketCard.jsx
@@ -1,10 +1,12 @@
 import React from "react";
 import DeleteBlock from "./DeleteBlock";
+import PriorityDisplay from "./PriorityDisplay";
 
 const TicketCard = () => {
   return (
     <div>
       <DeleteBlock />
+      <PriorityDisplay />
     </div>
   );
 };
