b85a0297b2e143eff95ff3e902bcb7290d9314cf
Pradikshan <pradikshan234@gmail.com>
2024-03-03 17:29:22 +0530

    Added updated logic

--- app/TicketPage/[id]/page.jsx
+++ app/TicketPage/[id]/page.jsx
@@ -1,7 +1,29 @@
 import TicketForm from "@/app/[components]/TicketForm";
 import React from "react";
 
-const TicketPage = ({ params }) => {
+const getTicketById = async (id) => {
+  try {
+    const res = await fetch(`http://localhost:3000/api/Tickets/${id}`, {
+      cache: "no-store",
+    });
+
+    if (!res.ok) {
+      throw new Error("Failed to get ticket");
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+const TicketPage = async ({ params }) => {
+  const EDITMODE = params.id === "new" ? false : true;
+
+  let updateTicketData = {};
+
+  if (EDITMODE) {
+    updateTicketData = await getTicketById(params.id);
+    console.log(updateTicketData);
+  }
   return <TicketForm />;
 };
 
