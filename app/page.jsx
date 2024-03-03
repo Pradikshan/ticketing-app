fc5c078a320ebfc4b8aa5cf0b1c9bb46cf413dbd
Pradikshan <pradikshan234@gmail.com>
2024-03-03 16:52:49 +0530

    Made minor change

--- app/page.jsx
+++ app/page.jsx
@@ -9,7 +9,7 @@ const getTickets = async () => {
 
     return res.json();
   } catch (error) {
-    console.log("Error: ", error);
+    console.log("failed to tickets", error);
   }
 };
 
@@ -24,25 +24,49 @@ const Dashboard = async () => {
     <div className="p-5">
       <div>
         {tickets &&
-          uniqueCategories?.map((uniqueCategory, categoryIndex) => {
+          uniqueCategories?.map((uniqueCategory, categoryIndex) => (
             <div key={categoryIndex} className="mb-4">
               <h2>{uniqueCategory}</h2>
               <div className="lg:grid grid-cols-2 xl:grid-cols-4">
                 {tickets
                   .filter((ticket) => ticket.category === uniqueCategory)
-                  .map((filteredTicket, _index) => {
+                  .map((filteredTicket, _index) => (
                     <TicketCard
                       id={_index}
                       key={_index}
                       ticket={filteredTicket}
-                    />;
-                  })}
+                    />
+                  ))}
               </div>
-            </div>;
-          })}
+            </div>
+          ))}
       </div>
     </div>
   );
 };
+
+// return (
+//   <div className="p-5">
+//     <div>
+//       {tickets &&
+//         uniqueCategories?.map((uniqueCategory, categoryIndex) => (
+//           <div key={categoryIndex} className="mb-4">
+//             <h2>{uniqueCategory}</h2>
+//             <div className="lg:grid grid-cols-2 xl:grid-cols-4">
+//               {tickets
+//                 .filter((ticket) => ticket.category === uniqueCategory)
+//                 .map((filteredTicket, _index) => (
+//                   <TicketCard
+//                     id={_index}
+//                     key={_index}
+//                     ticket={filteredTicket}
+//                   />
+//                 ))}
+//             </div>
+//           </div>
+//         ))}
+//     </div>
+//   </div>
+// );
 
 export default Dashboard;
