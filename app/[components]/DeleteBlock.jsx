40f2a559337e1381ed4690e50810da2c21110374
Pradikshan <pradikshan234@gmail.com>
2024-03-03 17:19:43 +0530

    Added 'use client' statement

--- app/[components]/DeleteBlock.jsx
+++ app/[components]/DeleteBlock.jsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { faX } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { useRouter } from "next/navigation";
