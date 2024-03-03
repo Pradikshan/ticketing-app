2a4f39841770303558ee8b865b0378cb35d9078f
Pradikshan <pradikshan234@gmail.com>
2024-03-03 14:30:32 +0530

    Set up progress input

--- app/[components]/TicketForm.jsx
+++ app/[components]/TicketForm.jsx
@@ -68,6 +68,71 @@ const TicketForm = () => {
           <option value={"Software problem"}>Software problem</option>
           <option value={"Project"}>Project</option>
         </select>
+
+        <label htmlFor="priority">Priority</label>
+        <div>
+          <input
+            id="priority-1"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={1}
+            checked={formData.priority == 1}
+          />
+          <label>1</label>
+
+          <input
+            id="priority-2"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={2}
+            checked={formData.priority == 2}
+          />
+          <label>2</label>
+
+          <input
+            id="priority-3"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={3}
+            checked={formData.priority == 3}
+          />
+          <label>3</label>
+
+          <input
+            id="priority-4"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={4}
+            checked={formData.priority == 4}
+          />
+          <label>4</label>
+
+          <input
+            id="priority-5"
+            name="priority"
+            type="radio"
+            onChange={handleChange}
+            value={5}
+            checked={formData.priority == 5}
+          />
+          <label>5</label>
+        </div>
+        <label htmlFor="progress">Progress</label>
+        <input
+          type="range"
+          id="progress"
+          name="progress"
+          value={formData.progress}
+          min={0}
+          max={100}
+          onChange={handleChange}
+        />
+        <label htmlFor="status">Status</label>
+        <select name="status" value={formData.status} onChange={}
       </form>
     </div>
   );
